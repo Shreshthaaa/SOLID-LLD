95fc1c29d82e09ca8000971998a6771eb0340a97
Shreshthaaa <paliwalshreshtha@gmail.com>
2025-09-08 17:49:51 +0530

    Update beverages_decorator

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -1,17 +1,14 @@
 package beverages_decorator;
 
 public class Client {
+    public static void main(String[] args) {
+        Beverage coffee = new Cappuccino();
+        System.out.println("Cappuccino cost: " + coffee.cost());
 
-	public static void main(String[] args) {
-		
+        Beverage coffeeWithMilk = new Milk(new Cappuccino());
+        System.out.println("Cappuccino + Milk cost: " + coffeeWithMilk.cost());
 
-		
-		Beverage coffee = new Cappuccino();
-        System.out.println("Coffee");
-		System.out.println(coffee.cost());
-		
-		
-
-	}
-
+        Beverage latteWithExtras = new Sugar(new Milk(new Latte()));
+        System.out.println("Latte + Milk + Sugar cost: " + latteWithExtras.cost());
+    }
 }
